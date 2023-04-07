ba063b6f1fc540287eadd483c5f1ab47f97ffe8d
akova88 <phuoc.fiit@gmail.com>
2023-04-07 11:43:56 +0700

    giai phuong trinh b1

---
+++ Hello_java/src/main/java/Linear.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Linear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("c: ");
+        double c = scanner.nextDouble();
+
+        if ( a!=0) {
+            double result = (c-b)/a;
+            System.out.println("Nghiem cua pt là: " + result);
+        } else {
+            if ( b == c) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        }
+    }
+}

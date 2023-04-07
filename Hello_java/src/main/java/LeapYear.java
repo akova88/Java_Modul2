7218de6fb0e87e50aaa8cf5a1a36f68baacf1f18
akova88 <phuoc.fiit@gmail.com>
2023-04-07 14:52:39 +0700

    Leap year

---
+++ Hello_java/src/main/java/LeapYear.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Nhập vào năm:");
+        year = scanner.nextInt();
+//        if (year % 4 == 0) {
+//            if ( year % 100 == 0) {
+//                if ( year % 400 == 0) {
+//                    System.out.printf("%d là năm nhuận", year);
+//                } else {
+//                    System.out.printf("%d khong phai la nam nhuan",year);
+//                }
+//            } else {
+//                System.out.printf("%d là năm nhuận", year);
+//            }
+//        } else {
+//            System.out.printf("%d khong phai la nam nhuan",year);
+//        }
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+    if (isLeapYear) {
+        System.out.printf("%d la nam nhuan", year);
+    } else {
+        System.out.printf("%d ko la nam nhuan", year);
+    }
+        }
+    }
+

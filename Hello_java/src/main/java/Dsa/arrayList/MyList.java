974a5bd89c0e3d3d70830a4370f7ce03cc79a5ce
akova88 <phuoc.fiit@gmail.com>
2023-04-23 21:31:57 +0700

    My List

---
+++ Hello_java/src/main/java/Dsa/arrayList/MyList.java
@@ -0,0 +1,16 @@
+package Dsa.arrayList;
+
+public interface MyList<E> extends java.lang.Iterable {
+    void add(E e);
+    void add(int index, E e);
+    void clear();
+    boolean contains(E e);
+    E get(int index);
+    int indexOf(E e);
+    boolean isEmpty();
+    int lastIndexOf(E e);
+    boolean remove(E e);
+    E remove(int index);
+    Object set(int index, E e);
+    int size();
+}

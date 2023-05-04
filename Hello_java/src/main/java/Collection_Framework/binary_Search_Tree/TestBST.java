52010714092ad6755cd920a8a95ca13d4e122166
akova88 <phuoc.fiit@gmail.com>
2023-05-04 09:37:29 +0700

    Algorithm Complexity

--- Hello_java/src/main/java/Collection_Framework/binary_Search_Tree/TestBST.java
+++ Hello_java/src/main/java/Collection_Framework/binary_Search_Tree/TestBST.java
@@ -16,20 +16,20 @@ public class TestBST {
 //        System.out.println("Inorder (sorted): ");
 ////        tree.inOrder();
 //        System.out.println("The number of nodes is: " + tree.getSize());
-////        tree.postOrder();
-//        tree.preOrder();
-        BST<Integer> tree1 = new BST<>();
-        tree1.insert(27);
-        tree1.insert(14);
-        tree1.insert(35);
-        tree1.insert(10);
-        tree1.insert(19);
-        tree1.insert(31);
-        tree1.insert(42);
-//        tree1.inOrder();
-        tree1.preOrder();
-        System.out.println(tree1.search(11));
-
-        System.out.println(tree1.delete(42));
+//        tree.postOrder();
+        tree.preOrder();
+//        BST<Integer> tree1 = new BST<>();
+//        tree1.insert(27);
+//        tree1.insert(14);
+//        tree1.insert(35);
+//        tree1.insert(10);
+//        tree1.insert(19);
+//        tree1.insert(31);
+//        tree1.insert(42);
+////        tree1.inOrder();
+//        tree1.preOrder();
+//        System.out.println(tree1.search(11));
+//
+//        System.out.println(tree1.delete(42));
     }
 }

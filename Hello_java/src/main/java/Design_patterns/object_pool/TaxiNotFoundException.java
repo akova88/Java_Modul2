0de721c058606d8393769859a4caa236b20f5ce1
akova88 <phuoc.fiit@gmail.com>
2023-11-17 09:25:06 +0700

    object pool

---
+++ Hello_java/src/main/java/Design_patterns/object_pool/TaxiNotFoundException.java
@@ -0,0 +1,7 @@
+package Design_patterns.object_pool;
+
+public class TaxiNotFoundException extends RuntimeException{
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}

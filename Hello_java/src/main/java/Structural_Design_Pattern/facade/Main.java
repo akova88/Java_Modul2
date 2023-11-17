89c350e6337e26dbd4199a9fa291f380ab4d8e2c
akova88 <phuoc.fiit@gmail.com>
2023-11-17 23:15:25 +0700

    facade

---
+++ Hello_java/src/main/java/Structural_Design_Pattern/facade/Main.java
@@ -0,0 +1,8 @@
+package Structural_Design_Pattern.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.share("This a post");
+    }
+}
---
+++ Hello_java/src/main/java/Structural_Design_Pattern/facade/SocialShare.java
@@ -0,0 +1,6 @@
+package Structural_Design_Pattern.facade;
+
+public interface SocialShare {
+    void setMessage(String message);
+    void share();
+}
